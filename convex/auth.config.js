421e03663f78694eb2d5c23045f77d5e4e5c10aa
Waseem Akram <hackerbad669@gmail.com>
2023-10-17 10:57:48 +0500

    Change auth url

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://actual-glider-21.clerk.accounts.dev",
+      domain: "https://unique-midge-19.clerk.accounts.dev",
       applicationID: "convex",
     }
   ]
